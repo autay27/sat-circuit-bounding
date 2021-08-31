9582de37d14102c9ee890659473b30b298f5e61c
autay27 <au.tay.27@gmail.com>
2021-08-31 19:24:41 +0100

    fixes to truth table input

--- decodeSolution.sh
+++ decodeSolution.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/sh
 
-# expected inputs are <problem file with var names and map> <SAT solution>
+# expected inputs are <problem file with var names> <SAT solution>
 
 PROB=$1
 SAT=$2
-echo "Problem file : ${PROB}"
+echo "Problem file: ${PROB}"
 echo "Solution file: ${SAT}"
 
 VARVAL=$(tempfile -p varval_)
 VARMAP=$(tempfile -p varmap_)
 
+#format the sat output to one variable per line
 cat ${SAT} | tr ' ' '\n' | grep -v SAT | egrep -v "^0$" > ${VARVAL}
-cat ${PROB} | sed '1,/index to name/ d' > ${VARMAP}
+#
+cat ${PROB} | sed '1,/variable/ d' > ${VARMAP}
 
 paste -d' ' ${VARMAP} ${VARVAL} | cut -d' ' -f4,5 | sed -E 's/ [0-9]+/ TRUE/' | sed 's/ -.*//'
 
+#wat is going on why does the final variable's name not get printed in the comments
